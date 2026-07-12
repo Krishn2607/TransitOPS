0078d0be783f351a1c49de2ffbb6e1c9f1e6a34d
DrashtiVarasada <varasadadrashti@gmail.com>
2026-07-12 11:39:07 +0530

    create admin.py in trip

--- TransitOps/trips/admin.py
+++ TransitOps/trips/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from .models import Trip, TripAssignment, TripStatusHistory
 
-# Register your models here.
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "trip_id",
+        "source",
+        "destination",
+        "status",
+        "start_datetime",
+    )
+
+    search_fields = (
+        "trip_id",
+        "source",
+        "destination",
+    )
+
+    list_filter = (
+        "status",
+    )
+
+    ordering = (
+        "-created_at",
+    )
+
+
+@admin.register(TripAssignment)
+class TripAssignmentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "trip",
+        "vehicle",
+        "driver",
+        "assigned_by",
+        "assigned_at",
+    )
+
+    search_fields = (
+        "trip__trip_id",
+        "vehicle__registration_number",
+        "driver__driver_name",
+    )
+
+    ordering = (
+        "-assigned_at",
+    )
+
+
+@admin.register(TripStatusHistory)
+class TripStatusHistoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "trip",
+        "old_status",
+        "new_status",
+        "changed_by",
+        "changed_at",
+    )
+
+    list_filter = (
+        "new_status",
+    )
+
+    ordering = (
+        "-changed_at",
+    )
