1dd477e379b04deff6e8391f52713a40761744d2
PruthaAghara <agharaprutha@gmail.com>
2026-07-12 14:15:22 +0530

    Added vehicle list and add vehicle functionality

--- TransitOps/fleet/urls.py
+++ TransitOps/fleet/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from .views import VehicleListView
+from .views import VehicleListView, vehicle_add
+
 
 urlpatterns = [
-    path("vehicles/", VehicleListView.as_view(), name="vehicle_list"),
+    path(
+        "vehicles/",
+        VehicleListView.as_view(),
+        name="vehicle_list"
+    ),
+
+    path(
+        "vehicles/add/",
+        vehicle_add,
+        name="vehicle_add"
+    ),
 ]
--- TransitOps/fleet/views.py
+++ TransitOps/fleet/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from .models import Vehicle
+from .forms import VehicleForm
 
 class VehicleListView(ListView):
     model = Vehicle
     template_name = "fleet/vehicle_list.html"
     context_object_name = "vehicles"
     paginate_by = 10
+
+def vehicle_add(request):
+
+    if request.method == "POST":
+
+        form = VehicleForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("vehicle_list")
+
+    else:
+
+        form = VehicleForm()
+
+
+    return render(
+        request,
+        "fleet/vehicle_add.html",
+        {
+            "form": form
+        }
+    )
