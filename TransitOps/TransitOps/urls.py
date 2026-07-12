80dac5670429924bf1978ea086089fd52c364e22
Mansvi-03 <mansvichovatiya@gmail.com>
2026-07-12 13:25:22 +0530

    dashboard

--- TransitOps/TransitOps/urls.py
+++ TransitOps/TransitOps/urls.py
@@ -6,15 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-<<<<<<< HEAD
     path("", include("accounts.urls")),
     path("", include("dashboard.urls")),
-=======
-    # Accounts App
-    path("", include("accounts.urls")),
->>>>>>> c6a23d4c9b4edc97ebe68609e9631b62cbb01c7a
-
-    # Fleet App
     path("fleet/", include("fleet.urls")),
 ]
 
