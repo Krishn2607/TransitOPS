2c1ab4aeafddb043b9f4e1f24d46a224b4068a66
Mansvi-03 <mansvichovatiya@gmail.com>
2026-07-12 09:59:41 +0530

    commit MANSVI

--- TransitOps/accounts/models.py
+++ TransitOps/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+
+    ROLE_CHOICES = [
+        ("Fleet Manager", "Fleet Manager"),
+        ("Dispatcher", "Dispatcher"),
+        ("Safety Officer", "Safety Officer"),
+        ("Financial Analyst", "Financial Analyst"),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+    phone = models.CharField(max_length=15, blank=True)
+    profile_image = models.ImageField(upload_to="profiles/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.role})"
